20adb200ed35cc56c68f39beb680501016c069d6
Saransh Dubey <57388472+saransh57@users.noreply.github.com>
2024-09-19 13:53:51 +0530

    Update Code_Rule3_crypto.swift

--- Code_Rule3_crypto.swift
+++ Code_Rule3_crypto.swift
@@ -26,8 +26,18 @@ rules:
     patterns:
       - pattern: |
           $OBJ.initialize($KEY_SIZE)
+    rules:
+  - id: insecure-cryptography
+    languages:
+      - scala
+    severity: ERROR
+    message: |
+      This line is using an insecure key size. Use at least 2048 bits for RSA.
+    patterns:
+      - pattern: $OBJ.initialize($KEY_SIZE)
     metavariable-regex:
       metavariable: $KEY_SIZE
-      regex: 1024
+      regex: ^(10[0-1][0-9]|10[2-9]\d|[5-9]\d\d|1\d\d\d)$
     fix: |
       $OBJ.initialize(2048)
+  
