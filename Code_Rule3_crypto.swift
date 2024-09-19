26c8f97d13621256874c12146981f2677055bb56
Saransh Dubey <57388472+saransh57@users.noreply.github.com>
2024-09-19 13:47:13 +0530

    Create Code_Rule3_crypto.swift

---
+++ Code_Rule3_crypto.swift
@@ -0,0 +1,33 @@
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.security.NoSuchProviderException
+
+object InsecureCryptographyNoncompliant {
+ 
+    @throws[NoSuchAlgorithmException]
+    @throws[NoSuchProviderException]
+    def weakKeySizeWithProviderString = {
+        
+        val keyGen = KeyPairGenerator.getInstance("RSA", "BC")
+       // ruleid: insecure-cryptography
+        keyGen.initialize(1024)
+        keyGen.generateKeyPair
+    }
+ 
+}
+
+rules:
+  - id: insecure-cryptography
+    languages:
+      - scala
+    severity: ERROR
+    message: |
+      This line is using an insecure key size. Use at least 2048 bits for RSA.
+    patterns:
+      - pattern: |
+          $OBJ.initialize($KEY_SIZE)
+    metavariable-regex:
+      metavariable: $KEY_SIZE
+      regex: 1024
+    fix: |
+      $OBJ.initialize(2048)
